b999fa374599015147c953df727dbfac28fa1f45
Arist0v <arist0v@disroot.org>
2022-04-16 09:40:35 -0400

    move to empty compose activity

--- app/src/androidTest/java/io/webthings/webthings/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/io/webthings/webthings/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package io.webthings.webthingsandroidapp
+package io.webthings.webthings
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("io.webthings.webthingsandroidapp", appContext.packageName)
+        assertEquals("io.webthings.webthings", appContext.packageName)
     }
 }
--- app/src/test/java/io/webthings/webthings/ExampleUnitTest.kt
+++ app/src/test/java/io/webthings/webthings/ExampleUnitTest.kt
@@ -1,4 +1,4 @@
-package io.webthings.webthingsandroidapp
+package io.webthings.webthings
 
 import org.junit.Test
 
