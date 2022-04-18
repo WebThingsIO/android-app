b7abcaa6a9fe374e9ded81a4fac7a9db571cb540
Arist0v <arist0v@disroot.org>
2022-04-18 16:43:47 -0400

    work on way to make gui

--- app/src/main/java/io/webthings/webthings/ui/NavRoutes.kt
+++ app/src/main/java/io/webthings/webthings/ui/NavRoutes.kt
@@ -1,6 +1,14 @@
 package io.webthings.webthings.ui
 
-sealed class NavRoutes(val route:String){
-    object HomeScreen : NavRoutes("homes")
-    object DashboardScreen: NavRoutes("Dashboard")
+import androidx.compose.ui.res.stringResource
+import io.webthings.webthings.R
+
+sealed class NavRoutes(val route:String, title:String){
+    object HomeScreen : NavRoutes("homes", stringResource(R.string.home))
+    object DashboardScreen: NavRoutes("Dashboard", stringResource(R.string.main_dashboard))
 }
+
+val drawerMenuItems = listOf(
+    //TODO add other drawer menu entry
+    NavRoutes.DashboardScreen
+)
