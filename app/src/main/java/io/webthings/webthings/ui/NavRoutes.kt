024fa043c0e2435bf32dbbbe2714a26571b436e0
arist0v <arist0v@disroot.org>
2022-04-19 12:39:06 -0400

    fix some bug

--- app/src/main/java/io/webthings/webthings/ui/NavRoutes.kt
+++ app/src/main/java/io/webthings/webthings/ui/NavRoutes.kt
@@ -1,12 +1,16 @@
 package io.webthings.webthings.ui
 
+import android.content.Context
+import android.content.res.Resources
 import androidx.compose.ui.res.stringResource
 import io.webthings.webthings.R
 import kotlin.math.absoluteValue
 
 sealed class NavRoutes(val route:String, val title:String){
-    object HomeScreen : NavRoutes("home", "Home")
-    object DashboardScreen: NavRoutes("Dashboard", "Main Dashboard")
+    object HomeScreen : NavRoutes("home",
+        "home")
+    object DashboardScreen: NavRoutes("Dashboard",
+        "Main Dashboard")
 }
 
 val drawerMenuItems = listOf(
