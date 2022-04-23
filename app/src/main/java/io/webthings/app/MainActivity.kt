c07f859d9e185c31fe0746210800c5d53051d530
Arist0v - dev <arist0v-dev@pop-os.localdomain>
2022-04-23 14:08:25 -0400

    renaming package

--- app/src/main/java/io/webthings/app/MainActivity.kt
+++ app/src/main/java/io/webthings/app/MainActivity.kt
@@ -1,4 +1,4 @@
-package io.webthings.webthings
+package io.webthings.app
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -10,7 +10,7 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import io.webthings.webthings.ui.theme.WebthingsTheme
+import io.webthings.app.ui.theme.WebthingsTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/io/webthings/app/ui/theme/Shape.kt
+++ app/src/main/java/io/webthings/app/ui/theme/Shape.kt
@@ -1,4 +1,4 @@
-package io.webthings.webthings.ui.theme
+package io.webthings.app.ui.theme
 
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Shapes
