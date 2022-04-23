eb5178871aff9028a8a6d7522dc7b2e25c235887
Arist0v - dev <arist0v-dev@pop-os.localdomain>
2022-04-23 14:16:38 -0400

    forgeted one

--- app/src/androidTest/java/io/webthings/app/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/io/webthings/app/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("io.webthings.webthings", appContext.packageName)
+        assertEquals("io.webthings.app", appContext.packageName)
     }
 }
