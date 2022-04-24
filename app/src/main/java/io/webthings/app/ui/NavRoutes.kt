60b1dc59d299eee7177a67c2b32cdde6e6ab7020
Arist0v - dev <arist0v-dev@pop-os.localdomain>
2022-04-23 21:47:46 -0400

    conflict issue

--- app/src/main/java/io/webthings/app/ui/NavRoutes.kt
+++ app/src/main/java/io/webthings/app/ui/NavRoutes.kt
@@ -1,21 +1,37 @@
-package io.webthings.webthings.ui
+package io.webthings.app.ui
+
 
 import android.content.Context
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Face
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.ui.graphics.vector.ImageVector
+import io.webthings.app.R
 import android.content.res.Resources
-import androidx.compose.ui.res.stringResource
-import io.webthings.webthings.R
-import kotlin.math.absoluteValue
+
+private val _context = Resources.getSystem()
 
 sealed class NavRoutes(val route:String, val title:String){
+
     object HomeScreen : NavRoutes("home",
-        "home")
-    object DashboardScreen: NavRoutes("Dashboard",
-        "Main Dashboard")
-    object GwSettingsScreen: NavRoutes("GwSettings", "Settings")
+        _context.getString(R.string.home))
+
+    sealed class DrawerItems(
+        route: String,
+        title: String,
+        val icon: ImageVector): NavRoutes(route, title){
+        object DashboardScreen: DrawerItems("Dashboard",
+            "Main Dashboard",
+            Icons.Default.Face)
+        object GwSettingsScreen: DrawerItems("GwSettings",
+            "Settings",
+            Icons.Default.Settings)
+    }
+
 }
 
 val drawerMenuItems = listOf(
     //TODO add other drawer menu entry
-    NavRoutes.DashboardScreen,
-    NavRoutes.GwSettingsScreen
+    NavRoutes.DrawerItems.DashboardScreen,
+    NavRoutes.DrawerItems.GwSettingsScreen
 )
