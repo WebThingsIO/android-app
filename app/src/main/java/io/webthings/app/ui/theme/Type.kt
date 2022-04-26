ef9e6cb748c55353f9f0c6eaf750562da31b3990
Arist0v <arist0v@disroot.org>
2022-04-26 11:50:25 -0400

    set app font and text style

--- app/src/main/java/io/webthings/app/ui/theme/Type.kt
+++ app/src/main/java/io/webthings/app/ui/theme/Type.kt
@@ -1,13 +1,24 @@
 package io.webthings.app.ui.theme
 
+import androidx.compose.material.Text
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import io.webthings.app.R
 
+fun WebthingsFont() = FontFamily(
+    Font(R.font.zilla_slab_medium)
+)
+
+fun TextFont() = FontFamily(
+    Font(R.font.open_sans)
+)
 // Set of Material typography styles to start with
 val Typography = Typography(
+    defaultFontFamily = TextFont(),
     body1 = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Normal,
@@ -25,4 +36,15 @@ val Typography = Typography(
         fontSize = 12.sp
     )
     */
+)
+val HeaderStyle = TextStyle(
+    fontFamily = WebthingsFont(),
+    fontWeight = FontWeight.Normal,
+    fontSize = 32.sp
+)
+
+val MenuItemStyle = TextStyle(
+    fontFamily = TextFont(),
+    fontWeight = FontWeight.Normal,
+    fontSize = 24.sp
 )
