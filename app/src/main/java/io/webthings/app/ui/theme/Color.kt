1c16060cf12113a25bce086878f8b9348d833a64
arist0v <arist0v@users.noreply.github.com>
2022-04-28 10:44:00 -0400

    Themes (#58)
    * added font to project
    
    * add color
    
    * settings themes
    
    * set app font and text style

--- app/src/main/java/io/webthings/app/ui/theme/Color.kt
+++ app/src/main/java/io/webthings/app/ui/theme/Color.kt
@@ -6,3 +6,8 @@ val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
+
+val LightBlue = Color(93,155,199)
+val DarkBlue = Color(72,120,154)
+val White = Color(255,255,255)
+val DarkWhite = Color(221,221,221)
