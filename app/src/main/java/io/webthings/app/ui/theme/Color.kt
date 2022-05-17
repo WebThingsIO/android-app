10d8b8494057cee40b3f4e197e3ce3be96a5bc14
arist0v <arist0v@users.noreply.github.com>
2022-05-16 20:15:49 -0400

    Update app/src/main/java/io/webthings/app/ui/theme/Color.kt
    Co-authored-by: Tim Hellhake <tim-hellhake@users.noreply.github.com>

--- app/src/main/java/io/webthings/app/ui/theme/Color.kt
+++ app/src/main/java/io/webthings/app/ui/theme/Color.kt
@@ -3,6 +3,6 @@ package io.webthings.app.ui.theme
 import androidx.compose.ui.graphics.Color
 
 val LightBlue = Color(93,155,199)
-val DarkBlue = Color(72, 120, 154)
+val DarkBlue = Color(72,120,154)
 val White = Color(255,255,255)
 val DarkWhite = Color(221,221,221)
