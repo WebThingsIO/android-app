bb27880f4853b37caa40bada510460c5b872e351
arist0v <arist0v@users.noreply.github.com>
2022-05-16 20:10:13 -0400

    Update app/src/main/java/io/webthings/app/templates/ThingsTemplates.kt
    Co-authored-by: Tim Hellhake <tim-hellhake@users.noreply.github.com>

--- app/src/main/java/io/webthings/app/templates/ThingsTemplates.kt
+++ app/src/main/java/io/webthings/app/templates/ThingsTemplates.kt
@@ -1,14 +1,14 @@
 package io.webthings.app.templates
 
+/**
+ * Class that represent a Things
+ *
+ * source - SourceTemplate child class represent the source who own this things
+ */
 abstract class ThingsTemplates(
     val source: SourceTemplate,
     val id: String,
     var name: String) {
-    /*
-     * Class that represent a Things
-     *
-     * source - SourceTemplate child class represent the source who own this things
-     */
 
 
 }
